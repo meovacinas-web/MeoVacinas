cb7c54f5a8b439f5810524a414ea9bee348d3aeb
meovacinas-web <meovacinas@gmail.com>
2026-03-09 01:16:34 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  signInWithPopup, 
-  signOut, 
-  onAuthStateChanged 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
 } from "firebase/auth";
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBAYAKfAUJYLensvAtPNiKeOgcAqBFKnJA",
+  apiKey: "AIzaSy...",
   authDomain: "meovacinas-6fecc.firebaseapp.com",
   projectId: "meovacinas-6fecc",
   storageBucket: "meovacinas-6fecc.firebasestorage.app",
@@ -26,10 +26,16 @@ const app = initializeApp(firebaseConfig);
 // Serviços
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+
+// Analytics
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+export { analytics };
 
 // Login Google
 export const googleProvider = new GoogleAuthProvider();
 
-// Exportar funções úteis
+// Exportar funções
 export { signInWithPopup, signOut, onAuthStateChanged };
